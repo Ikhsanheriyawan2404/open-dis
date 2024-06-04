3fbcbe29593fb9af906fa590c6d4a48c21d2fdb4
ikhsanheriyawan <ikhsanheriyawan2404@gmail.com>
2024-06-04 16:46:46 +0700

    First Commit

--- src/dis-udpclient.js
+++ src/dis-udpclient.js
@@ -5,8 +5,8 @@ const DISUtils = require('./DISUtils');
 
 var utils = new DISUtils();
 
-var DEFAULT_HOST = '127.0.0.1';
-const DEFAULT_PORT = 3000
+var DEFAULT_HOST = 'localhost';
+const DEFAULT_PORT = 4000
 
 /**
  * Read host & port as commandline arguments - or take the default if not given:
@@ -18,12 +18,61 @@ var port = argv.port || DEFAULT_PORT
 var client = dgram.createSocket('udp4');        /** Open a UDP-Client */
 
 /**
+ * Create a function to initialize a new EntityStatePDU for an aircraft:
+ */
+function createEntityStatePDU(site, application, entity, markingText) {
+    var disEntityStatePDU= new dis.EntityStatePdu()
+    disEntityStatePDU.entityID.site = site;
+    disEntityStatePDU.entityID.application = application;
+    disEntityStatePDU.entityID.entity = entity;
+    disEntityStatePDU.marking.setMarking(markingText);
+    return disEntityStatePDU;
+}
+
+/**
+ * Initial positions for two aircraft:
+ */
+var aircraft1 = createEntityStatePDU(11, 22, 33, "F-16A");
+var aircraft2 = createEntityStatePDU(11, 22, 34, "HAWK MK53");
+
+var targetPosition1 = { x: 1000, y: 0, z: 0 };
+var targetPosition2 = { x: 1100, y: 0, z: 0 };
+
+/**
+ * Function to update position towards target:
+ */
+function updatePosition(entity, targetPosition) {
+    var speed = 10; // Units per update
+    if (entity.entityLocation.x < targetPosition.x) {
+        entity.entityLocation.x += speed;
+    }
+    if (entity.entityLocation.y < targetPosition.y) {
+        entity.entityLocation.y += speed;
+    }
+    if (entity.entityLocation.z < targetPosition.z) {
+        entity.entityLocation.z += speed;
+    }
+}
+
+/**
+ * Function to send EntityStatePDU:
+ */
+function sendEntityStatePDU(entityStatePDU) {
+    var message = utils.DISPduToBuffer(entityStatePDU);
+    client.send(message, 0, message.length, port, host, function(err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + host + ':' + port);
+    });
+}
+
+/**
  * Create a Dummy EntityState-PDU and fill it with dummy data - just for testing:
  */
 var disEntityStatePDU= new dis.EntityStatePdu()
 disEntityStatePDU.entityID.site = 11;
 disEntityStatePDU.entityID.application = 22;
 disEntityStatePDU.entityID.entity = 33;
+disEntityStatePDU.entityLocation = new dis.Vector3Float(1, 2, 3);
 disEntityStatePDU.marking.setMarking("Example Entity")
 
 
@@ -45,4 +94,38 @@ client.send(message, 0, message.length, port, host, function(err, bytes) {
     if (err) throw err;
     console.log('UDP message sent to ' + host +':'+ port);
     client.close()
+});
+
+/**
+ * Function to simulate the movement and send data:
+ */
+function simulateMovement() {
+    for (let i = 0; i < aircraft1.entityLocation.x; i++) {
+        updatePosition(aircraft1, targetPosition1);
+        console.log("Sending EntityState for Aircraft 1:", aircraft1.entityID, "Location", aircraft1.entityLocation, "Marking: \'" + aircraft1.marking.getMarking() + "\'");
+        sendEntityStatePDU(aircraft1);
+    }
+
+    for (let i = 0; i < aircraft2.entityLocation.x; i++) {
+        updatePosition(aircraft2, targetPosition2);
+        console.log("Sending EntityState for Aircraft 2:", aircraft2.entityID, "Location", aircraft2.entityLocation, "Marking: \'" + aircraft2.marking.getMarking() + "\'");
+        sendEntityStatePDU(aircraft2);
+    }
+
+    // client.close(() => {
+    //     console.log('UDP client closed');
+    // });
+}
+
+simulateMovement();
+
+/**
+ * Close the client when process is terminated:
+ */
+process.on('SIGINT', function() {
+    clearInterval(interval); // Clear the interval to stop sending data
+    client.close(() => {
+        console.log('\nUDP client closed');
+        process.exit();
+    });
 });
--- src/dis-udpserver.js
+++ src/dis-udpserver.js
@@ -1,71 +1,176 @@
 const dgram = require('dgram');
-const argv = require('yargs').argv;
-const dis = require("open-dis")
+const express = require('express');
+const http = require('http');
+const socketIo = require('./socket.io');
+const cors = require('cors'); // Import modul CORS
+const dis = require("open-dis");
 const DISUtils = require('./DISUtils');
+require('dotenv').config();
 
-var utils = new DISUtils();
+let utils = new DISUtils();
 
-
-const DEFAULT_PORT = 3000
-
-/**
- * Read port as commandline arguments - or take the default if not given:
- */
-var port = argv.port || DEFAULT_PORT
-
-
-var server = dgram.createSocket('udp4');
+const DEFAULT_HOST = '127.0.0.1';
+const DEFAULT_PORT = 4000
+const DEFAULT_UDP_PORT = 4000;
+const DEFAULT_HTTP_PORT = 4000;
 
 /**
- * Messagehandler
- * 
+ * Read host & port as commandline arguments - or take the default if not given:
  */
-server.on('message', (msg, rinfo) => {
-  console.log(`server got msg from ${rinfo.address}:${rinfo.port}`);
+const host = process.env.HOST || DEFAULT_HOST
+const port = process.env.PORT || DEFAULT_PORT
 
-  try
-  {
-    var disMessage = utils.DISObjectFromBuffer(msg);
-    switch(disMessage.pduType)
-    {
-        case 1: // EntityState PDU:
-            var entityID = disMessage.entityID;
-            var location = disMessage.entityLocation;
-            var marking  = disMessage.marking.getMarking();
+/**
+ * Read ports as commandline arguments - or take the default if not given:
+ */
+const udpPort = process.env.UDP_PORT || DEFAULT_UDP_PORT;
+const httpPort = process.env.HTTP_PORT || DEFAULT_HTTP_PORT;
 
-            console.log("Got EntityState:", entityID, "Location", location, "Marking: \'" + marking + "\'"             )
-           
-            break;
-        case 20: // Data PDU:
-            console.log("Got DataPDU:")
-            break;
-        default:
-            console.log("Got Other PDU:", )
-    }
-  }catch(e)
-  {
-      console.log("Exception:",disMessage.pduType)
-  }
+
+/**
+ * Create an HTTP Server:
+ */
+const app = express();
+app.use(cors()); // Terapkan middleware CORS
+const server = http.createServer(app);
+const io = socketIo(server);
+
+/**
+ * Create a UDP Server:
+ */
+let udpServer = dgram.createSocket('udp4');
+
+io.on('connection', (socket) => {
+    console.log('Socket.IO client connected');
+    
+    socket.on('disconnect', () => {
+        console.log('Socket.IO client disconnected');
+    });
  
+    socket.on('error', (error) => {
+        console.error('Socket.IO error:', error);
+    });
+
+    socket.on('sendEntityState', (data) => {
+        let result = createEntityStatePDU(
+            data.site,
+            data.application,
+            data.entity,
+            data.markingText,
+            data.y,
+            data.x,
+            data.z,
+        );
+        sendEntityStatePDU(result)
+    });
+
 });
 
 /**
- * Some debug message to show errors
+ * Create a function to initialize a new EntityStatePDU for an aircraft:
  */
-server.on('error', (err) => {
-    console.log(`server error:\n${err.stack}`);
-    server.close();
-  });
+function createEntityStatePDU(site, application, entity, markingText = "Entity1", lat, lng, alt) {
+    let disEntityStatePDU = new dis.EntityStatePdu()
+    disEntityStatePDU.entityID.site = site;
+    disEntityStatePDU.entityID.application = application;
+    disEntityStatePDU.entityID.entity = entity;
+
+    // Inisialisasi objek Marking
+    disEntityStatePDU.marking = new dis.Marking();
+    disEntityStatePDU.marking.setMarking(markingText);
+
+    // Set entityLocation sebagai instance dari Vector3Float
+    // let { x, y, z } = latLngAltToECEF(lat, lng, alt);
+    // disEntityStatePDU.entityLocation = new dis.Vector3Float();
+    disEntityStatePDU.entityLocation.x = lng;
+    disEntityStatePDU.entityLocation.y = lat;
+    disEntityStatePDU.entityLocation.z = alt;
+
+    return disEntityStatePDU;
+}
 
 /**
- * Some debug message to show, when the server started listening
+ * Function to convert latitude, longitude, altitude to ECEF coordinates:
  */
-server.on('listening', () => {
-  const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
+function latLngAltToECEF(lat, lon, alt) {
+    const a = 6378137.0; // Equatorial radius in meters
+    const e2 = 6.69437999014e-3; // Square of eccentricity
+
+    lat = lat * Math.PI / 180.0;
+    lon = lon * Math.PI / 180.0;
+
+    const N = a / Math.sqrt(1 - e2 * Math.sin(lat) * Math.sin(lat));
+
+    const x = (N + alt) * Math.cos(lat) * Math.cos(lon);
+    const y = (N + alt) * Math.cos(lat) * Math.sin(lon);
+    const z = ((1 - e2) * N + alt) * Math.sin(lat);
+
+    return { x, y, z };
+}
+
+/**
+ * Function to send EntityStatePDU:
+ */
+function sendEntityStatePDU(entityStatePDU) {
+    let message = utils.DISPduToBuffer(entityStatePDU);
+    udpServer.send(message, 0, message.length, port, host, function(err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + host + ':' + port);
+    });
+}
+
+/**
+ * Message handler for UDP server
+ */
+udpServer.on('message', (msg, rinfo) => {
+    console.log(`UDP server got message from ${rinfo.address}:${rinfo.port}`);
+
+    try {
+        let disMessage = utils.DISObjectFromBuffer(msg);
+        switch(disMessage.pduType) {
+            case 1: // EntityState PDU:
+                let entityID = disMessage.entityID;
+                let location = disMessage.entityLocation;
+                let marking  = disMessage.marking.getMarking();
+                // console.log(disMessage)
+                console.log("Got EntityState:", entityID, "Location", location, "Marking: \'" + marking + "\'");
+
+                // Broadcast the message to all Socket.IO clients
+                io.emit('receiveEntityState', {
+                    entityID: entityID,
+                    location: location,
+                    marking: marking
+                });
+                break;
+            case 20: // Data PDU:
+                console.log("Got DataPDU:");
+                break;
+            default:
+                console.log("Got Other PDU:", disMessage.pduType);
+        }
+    } catch (e) {
+        console.log("Exception:", e);
+    }
 });
 
 /**
- * Actually start listening on port:
+ * Debug messages for UDP server
  */
-server.bind(port);
+udpServer.on('error', (err) => {
+    console.log(`UDP server error:\n${err.stack}`);
+    udpServer.close();
+});
+
+/**
+ * Start listening on UDP port
+ */
+udpServer.bind(udpPort, () => {
+    console.log(`UDP server listening on ${host}:${udpPort}`);
+});
+
+/**
+ * Start HTTP and Socket.IO server
+ */
+server.listen(httpPort, () => {
+    console.log(`HTTP and Socket.IO server listening on ${host}:${httpPort}`);
+});
